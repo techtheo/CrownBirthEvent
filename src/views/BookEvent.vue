478164c17e1e48138f6840b5e186fe745880d934
Tech Theo <theophilustemilade@gmail.com>
2024-08-30 14:11:37 +0100

    added new features

--- src/views/BookEvent.vue
+++ src/views/BookEvent.vue
@@ -24,23 +24,13 @@
     <!-- Date picker -->
     <div class="form-group">
       <label for="datetimepicker">Select Date:</label>
-      <input
-        id="datetimepicker"
-        type="text"
-        placeholder="Select a date"
-        class="flatpickr-input"
-      />
+      <input id="datetimepicker" type="text" placeholder="Select a date" class="flatpickr-input" />
     </div>
 
     <!-- Time picker -->
     <div class="form-group">
       <label for="timepicker">Select Time:</label>
-      <input
-        id="timepicker"
-        type="text"
-        placeholder="Select a time"
-        class="flatpickr-input"
-      />
+      <input id="timepicker" type="text" placeholder="Select a time" class="flatpickr-input" />
     </div>
 
     <!-- Additional form fields -->
@@ -91,34 +81,74 @@
       type="button"
       @click="bookEvent"
       :disabled="!isFormValid"
-      :class="['book-event-button', { 'disabled': !isFormValid }]"
+      :class="['book-event-button', { disabled: !isFormValid }]"
     >
       Book Event
     </button>
 
     <!-- Proceed to Payment button (hidden initially) -->
-    <button
-      v-if="bookingSuccess"
-      type="button"
-      @click="proceedToPayment"
-      class="proceed-button"
-    >
+    <button v-if="bookingSuccess" type="button" @click="proceedToPayment" class="proceed-button">
       Proceed to Payment
     </button>
 
     <div v-if="loading" class="spinner">Loading...</div>
+
+    <!-- Payment Modal -->
+    <v-dialog v-model="showModal" max-width="600px">
+      <v-card class="receipt-card">
+        <v-card-title class="headline receipt-title">Payment Receipt</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="receipt-content">
+          <div class="receipt-item">
+            <strong>Event Space:</strong> {{ bookingDetails.eventSpaceName }}
+          </div>
+          <div class="receipt-item"><strong>Date:</strong> {{ bookingDetails.date }}</div>
+          <div class="receipt-item"><strong>Time:</strong> {{ bookingDetails.time }}</div>
+          <div class="receipt-item">
+            <strong>Event Type:</strong> {{ bookingDetails.eventType }}
+          </div>
+          <div class="receipt-item">
+            <strong>Number of Guests:</strong> {{ bookingDetails.guests }}
+          </div>
+          <div class="receipt-item"><strong>Duration:</strong> {{ bookingDetails.duration }}</div>
+          <div class="receipt-item">
+            <strong>Amount Paid:</strong> {{ formatCurrency(bookingDetails.amount) }}
+          </div>
+          <div class="receipt-item">
+            <strong>Payment Reference:</strong> {{ bookingDetails.paymentReference }}
+          </div>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions class="receipt-actions">
+          <v-btn color="primary" @click="printReceipt" class="receipt-button">Print Receipt</v-btn>
+          <v-btn @click="reloadPage" class="receipt-button">Close</v-btn>
+
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick, computed } from 'vue';
-import { getFirestore, collection, getDocs, addDoc, query, where, onSnapshot, doc, getDoc, updateDoc } from 'firebase/firestore';
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.css';
-import { useRouter } from 'vue-router';
-import { getAuth } from 'firebase/auth';
+import { ref, onMounted, nextTick, computed } from 'vue'
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  where,
+  onSnapshot,
+  doc,
+  getDoc,
+  updateDoc
+} from 'firebase/firestore'
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.css'
+import { useRouter } from 'vue-router'
+import { getAuth } from 'firebase/auth'
 
-const paystackScriptLoaded = ref(false);
+const paystackScriptLoaded = ref(false)
 
 const formData = ref({
   eventSpace: '',
@@ -128,98 +158,110 @@ const formData = ref({
   guests: '',
   duration: 'full-day',
   email: ''
-});
-const eventSpaces = ref([]);
-const bookedDates = ref([]);
-const bookedTimes = ref([]);
-const loading = ref(false);
-const bookingSuccess = ref(false);
-const selectedEventSpacePrice = ref(0);
-const bookingId = ref(''); // Store the booking ID for later use
+})
+const eventSpaces = ref([])
+const bookedDates = ref([])
+const bookedTimes = ref([])
+const loading = ref(false)
+const bookingSuccess = ref(false)
+const selectedEventSpacePrice = ref(0)
+const bookingId = ref('') // Store the booking ID for later use
 
-const db = getFirestore();
-const router = useRouter();
+const showModal = ref(false) // Control the visibility of the modal
+const bookingDetails = ref({}) // Store booking details to display in the modal
+
+const db = getFirestore()
+const router = useRouter()
 
 const fetchEventSpaces = async () => {
   try {
-    const spacesQuery = collection(db, 'event_spaces');
-    const querySnapshot = await getDocs(spacesQuery);
-    eventSpaces.value = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    const spacesQuery = collection(db, 'event_spaces')
+    const querySnapshot = await getDocs(spacesQuery)
+    eventSpaces.value = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching data:', error)
   }
-};
+}
 
 const updateDatePicker = () => {
-  flatpickr("#datetimepicker", {
-    dateFormat: "Y-m-d",
-    minDate: "today",
-    disable: bookedDates.value.map(date => new Date(date)),
+  flatpickr('#datetimepicker', {
+    dateFormat: 'Y-m-d',
+    minDate: 'today',
+    disable: bookedDates.value.map((date) => new Date(date)),
     onChange: (selectedDates) => {
-      formData.value.date = selectedDates[0] ? selectedDates[0].toISOString().split('T')[0] : '';
-      fetchBookedDatesAndTimes(); // Refresh booked dates after selecting a date
+      formData.value.date = selectedDates[0] ? selectedDates[0].toISOString().split('T')[0] : ''
+      fetchBookedDatesAndTimes() // Refresh booked dates after selecting a date
     }
-  });
-};
+  })
+}
 
 const fetchBookedDatesAndTimes = () => {
-  const bookingsQuery = collection(db, 'bookings');
+  const bookingsQuery = collection(db, 'bookings')
   const q = query(
     bookingsQuery,
     where('eventSpaceId', '==', formData.value.eventSpace),
     where('status', '==', 'pending')
-  );
+  )
 
   onSnapshot(q, (querySnapshot) => {
-    bookedDates.value = [];
-    bookedTimes.value = [];
-    querySnapshot.forEach(doc => {
-      const data = doc.data();
-      bookedDates.value.push(data.date);
-      bookedTimes.value.push(data.time);
-    });
-    updateDatePicker(); // Refresh the date picker with updated booked dates
-  });
-};
+    bookedDates.value = []
+    bookedTimes.value = []
+    querySnapshot.forEach((doc) => {
+      const data = doc.data()
+      bookedDates.value.push(data.date)
+      bookedTimes.value.push(data.time)
+    })
+    updateDatePicker() // Refresh the date picker with updated booked dates
+  })
+}
 
 const bookEvent = async () => {
   if (!isFormValid) {
-    alert("Please fill out all required fields.");
-    return;
+    alert('Please fill out all required fields.')
+    return
   }
 
-  loading.value = true;
+  loading.value = true
   try {
-    const user = getAuth().currentUser;
+    const user = getAuth().currentUser
     if (!user) {
-      console.error("User is not authenticated.");
-      loading.value = false;
-      return;
+      console.error('User is not authenticated.')
+      loading.value = false
+      return
     }
 
-    const userId = user.uid;
+    const userId = user.uid
 
     // Check if the event space is already booked at the selected date and time
-    const bookingsQuery = collection(db, 'bookings');
+    const bookingsQuery = collection(db, 'bookings')
     const q = query(
       bookingsQuery,
       where('eventSpaceId', '==', formData.value.eventSpace),
       where('date', '==', formData.value.date),
       where('time', '==', formData.value.time)
-    );
+    )
 
-    const querySnapshot = await getDocs(q);
+    const querySnapshot = await getDocs(q)
 
     if (!querySnapshot.empty) {
       // Event space is already booked at the selected date and time
-      alert("This event space is already booked for the selected date and time. Please choose another slot.");
-      loading.value = false;
-      return;
+      alert(
+        'This event space is already booked for the selected date and time. Please choose another slot.'
+      )
+      loading.value = false
+      return
     }
+
+    // Retrieve the selected event space details
+    const selectedSpace = eventSpaces.value.find((space) => space.id === formData.value.eventSpace)
+    const { name, image, price } = selectedSpace
 
     // Proceed with the booking
     const bookingRef = await addDoc(collection(db, 'bookings'), {
       eventSpaceId: formData.value.eventSpace,
+      eventSpaceName: name, // Save the event space name
+      eventSpaceImage: image, // Save the event space image URL
+      amount: price, // Save the amount
       date: formData.value.date,
       time: formData.value.time,
       eventType: formData.value.eventType,
@@ -230,29 +272,28 @@ const bookEvent = async () => {
       status: 'pending', // Initial status
       paymentStatus: 'unpaid', // New field for payment status
       paymentReference: '' // New field for payment reference
-    });
+    })
 
     // Store the booking ID
-    bookingId.value = bookingRef.id;
+    bookingId.value = bookingRef.id
 
-    // Retrieve the selected event space price
-    const eventSpaceDoc = await getDoc(doc(db, 'event_spaces', formData.value.eventSpace));
-    selectedEventSpacePrice.value = eventSpaceDoc.data().price;
+    // Store the selected event space price for payment
+    selectedEventSpacePrice.value = price
 
-    bookingSuccess.value = true;
+    bookingSuccess.value = true
   } catch (error) {
-    console.error('Booking error:', error);
+    console.error('Booking error:', error)
   } finally {
-    loading.value = false;
+    loading.value = false
   }
-};
+}
 
 const updatePaymentStatus = (reference) => {
   return updateDoc(doc(db, 'bookings', bookingId.value), {
     paymentStatus: 'paid',
     paymentReference: reference
-  });
-};
+  })
+}
 
 const proceedToPayment = () => {
   const handler = PaystackPop.setup({
@@ -263,64 +304,137 @@ const proceedToPayment = () => {
     ref: 'CB' + Math.floor(Math.random() * 1000000000 + 1), // Reference should be unique for every transaction
     callback: function (response) {
       // Payment complete! Record the reference in the backend
-      updatePaymentStatus(response.reference).then(() => {
-        alert('Payment complete! Reference: ' + response.reference);
-        router.push('/confirmation'); // Navigate to confirmation page
-      }).catch(error => {
-        console.error('Error updating payment status:', error);
-      });
+      updatePaymentStatus(response.reference)
+        .then(() => {
+          // Store the booking details for display in the modal
+          bookingDetails.value = {
+            ...formData.value,
+            eventSpaceName: eventSpaces.value.find(
+              (space) => space.id === formData.value.eventSpace
+            ).name,
+            amount: selectedEventSpacePrice.value,
+            paymentReference: response.reference
+          }
+          showModal.value = true // Show the modal with payment details
+        })
+        .catch((error) => {
+          console.error('Error updating payment status:', error)
+        })
     },
     onClose: function () {
-      alert('Transaction was not completed, window closed.');
+      alert('Transaction was not completed, window closed.')
     }
-  });
-  handler.openIframe();
-};
+  })
+  handler.openIframe()
+}
 
 onMounted(async () => {
-  await fetchEventSpaces();
-  await fetchBookedDatesAndTimes();
-  await nextTick();
+  await fetchEventSpaces()
+  await fetchBookedDatesAndTimes()
+  await nextTick()
 
   // Initialize flatpickr for date selection
-  updateDatePicker();
+  updateDatePicker()
 
-  flatpickr("#timepicker", {
+  flatpickr('#timepicker', {
     enableTime: true,
     noCalendar: true,
-    dateFormat: "H:i",
-    minTime: "08:00",
-    maxTime: "19:00",
+    dateFormat: 'H:i',
+    minTime: '08:00',
+    maxTime: '19:00',
     disable: bookedTimes.value,
     onChange: (selectedDates) => {
-      formData.value.time = selectedDates[0] ? selectedDates[0].toTimeString().split(' ')[0] : '';
+      formData.value.time = selectedDates[0] ? selectedDates[0].toTimeString().split(' ')[0] : ''
     }
-  });
+  })
 
   // Dynamically load Paystack script
-  const script = document.createElement('script');
-  script.src = "https://js.paystack.co/v1/inline.js";
-  script.async = true;
-  script.onload = () => paystackScriptLoaded.value = true; // Set to true when the script loads
+  const script = document.createElement('script')
+  script.src = 'https://js.paystack.co/v1/inline.js'
+  script.async = true
+  script.onload = () => (paystackScriptLoaded.value = true) // Set to true when the script loads
   script.onerror = () => {
-    console.error('Failed to load Paystack script.');
-    paystackScriptLoaded.value = false;
-  };
-  document.body.appendChild(script);
-});
+    console.error('Failed to load Paystack script.')
+    paystackScriptLoaded.value = false
+  }
+  document.body.appendChild(script)
+})
 
 const selectEventSpace = (spaceId) => {
-  formData.value.eventSpace = spaceId;
-};
+  formData.value.eventSpace = spaceId
+}
 
 const isFormValid = computed(() => {
-  return formData.value.eventSpace && formData.value.date && formData.value.time &&
-    formData.value.eventType && formData.value.guests && formData.value.email;
-});
+  return (
+    formData.value.eventSpace &&
+    formData.value.date &&
+    formData.value.time &&
+    formData.value.eventType &&
+    formData.value.guests &&
+    formData.value.email
+  )
+})
 
 const formatCurrency = (amount) => {
-  return new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN' }).format(amount);
+  return new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN' }).format(amount)
+}
+
+// Add the reloadPage function within your <script setup> section
+const reloadPage = () => {
+  showModal.value = false; // Close the modal
+
+  // Optionally, reload the page if you want to reset the form or booking process
+  // location.reload();
 };
+
+const printReceipt = () => {
+  const receiptContent = `
+    <html>
+      <head>
+        <title>Print Receipt</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+          }
+          h1 {
+            text-align: center;
+          }
+          .receipt-item {
+            margin-bottom: 10px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Payment Receipt</h1>
+        <div class="receipt-item"><strong>Event Space:</strong> ${bookingDetails.value.eventSpaceName}</div>
+        <div class="receipt-item"><strong>Date:</strong> ${bookingDetails.value.date}</div>
+        <div class="receipt-item"><strong>Time:</strong> ${bookingDetails.value.time}</div>
+        <div class="receipt-item"><strong>Event Type:</strong> ${bookingDetails.value.eventType}</div>
+        <div class="receipt-item"><strong>Number of Guests:</strong> ${bookingDetails.value.guests}</div>
+        <div class="receipt-item"><strong>Duration:</strong> ${bookingDetails.value.duration}</div>
+        <div class="receipt-item"><strong>Amount Paid:</strong> ${formatCurrency(bookingDetails.value.amount)}</div>
+        <div class="receipt-item"><strong>Payment Reference:</strong> ${bookingDetails.value.paymentReference}</div>
+      </body>
+    </html>
+  `;
+
+  const printWindow = window.open('', '_blank', 'width=800,height=600');
+  printWindow.document.open();
+  printWindow.document.write(receiptContent);
+  printWindow.document.close();
+
+  // Add a delay before printing and closing the window
+  printWindow.onload = () => {
+    printWindow.focus();
+    printWindow.print();
+
+    setTimeout(() => {
+      printWindow.close();
+    }, 1000); // 1 second delay to ensure printing completes
+  };
+};
+
 </script>
 
 <style scoped>
@@ -336,6 +450,12 @@ const formatCurrency = (amount) => {
 .event-card {
   margin: 10px;
   cursor: pointer;
+  transition: transform 0.2s ease, box-shadow 0.2s ease;
+}
+
+.event-card:hover {
+  transform: scale(1.05);
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
 }
 
 .event-card.selected {
@@ -358,6 +478,11 @@ const formatCurrency = (amount) => {
   color: white;
   border: none;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.book-event-button:hover {
+  background-color: #36a372;
 }
 
 .book-event-button.disabled {
@@ -371,10 +496,61 @@ const formatCurrency = (amount) => {
   color: white;
   border: none;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.proceed-button:hover {
+  background-color: #e44e4e;
 }
 
 .spinner {
   text-align: center;
   font-size: 18px;
 }
+
+.receipt-card {
+  padding: 20px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+}
+
+.receipt-title {
+  font-size: 24px;
+  font-weight: bold;
+  color: #2c3e50;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.receipt-content {
+  font-size: 16px;
+  color: #34495e;
+}
+
+.receipt-item {
+  margin-bottom: 10px;
+}
+
+.receipt-actions {
+  justify-content: center;
+}
+
+.receipt-button {
+  padding: 10px 10px;
+  font-size: 16px;
+  border-radius: 4px;
+  background-color: #42b983;
+  color: white;
+  transition: background-color 0.3s ease;
+}
+
+.receipt-button:hover {
+  background-color: #36a372;
+}
+
+.v-divider {
+  margin: 20px 0;
+}
+
 </style>
